c7b910b5dc79aa061ed1a9b21a98042f6bf79cf2
alex-baugh <alexander.baugh@cabinetoffice.gov.uk>
2024-12-12 20:23:01 +0000

    fix: modifiy menu

--- gocardlessosm/streamlit_app.py
+++ gocardlessosm/streamlit_app.py
@@ -8,10 +8,7 @@ from gocardlessosm.payout_processor import process_gocardless_export
 st.set_page_config(
     page_title="GoCardless Payouts",
     menu_items={
-        "Get Help": "https://www.extremelycoolapp.com/help",
-        "Report a bug": "https://www.extremelycoolapp.com/bug",
         "About": "# This is a header. This is an *extremely* cool app!",
-        "yay": "we are working!",
     },
 )
 
